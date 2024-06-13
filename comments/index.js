41c2b4e77bc50a24461ec9f98e32f39779b6d15c
YashRajSingh <yash13raj.singh@gmail.com>
2024-06-14 00:48:53 +0530

    events created

--- comments/index.js
+++ comments/index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
-app.use(cors());
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
@@ -13,21 +14,32 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
 
-  // Fetch or initialize the comments array for the given post ID
   const comments = commentsByPostId[req.params.id] || [];
 
-  // Add the new comment to the comments array
   comments.push({ id: commentId, content });
 
-  // Update the commentsByPostId object
   commentsByPostId[req.params.id] = comments;
 
-  // Send the response with status 201 and the comments array
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
+
   res.status(201).send(comments);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Event Received", req.body.type);
+
+  res.send({});
 });
 
 app.listen(4001, () => {
