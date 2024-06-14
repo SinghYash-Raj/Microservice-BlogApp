5fdf5c568c393ad103e09e2ad89524ba745b5136
YashRajSingh <yash13raj.singh@gmail.com>
2024-06-14 16:14:31 +0530

    comment moderation service

---
+++ moderation/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+//Whenever the event bus creates or receives a comment, it sends it out to the moderation service
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "CommentCreated") {
+    const status = data.content.includes("green") ? "rejected" : "approved";
+
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentModerated",
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content,
+      },
+    });
+  }
+});
+
+app.listen(4003, () => {
+  console.log("Listening on 4003");
+});
